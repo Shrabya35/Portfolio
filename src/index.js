ae0208a8e122e677fb4ca59c099c3a8e4f01c1da
Shrabya <97744118+Shrabya35@users.noreply.github.com>
2024-02-21 20:17:41 +0545

    Update index.js

--- src/index.js
+++ src/index.js
@@ -15,3 +15,16 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+//Just 
+//Random
+//Stuff
+//To
+//Make
+//JS : CSS
+//Ratio
+//Better
+//Hope 
+//This
+//help
